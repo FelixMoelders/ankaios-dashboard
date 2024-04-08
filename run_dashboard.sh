3ca1b3467b128fc93e5c7f3cbc8385862c4e12fb
om-oz <165789977+om-oz@users.noreply.github.com>
2024-04-08 14:57:18 +0000

    Revert "Changed shebang in 'run_dashboard.sh'"
    This reverts commit 8ccd8b228dcf51a0793bf2d8a317d69cde5eafe4.

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
--- stop_dashboard.sh
+++ stop_dashboard.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Cleanup Ankaios ....
 echo "Cleaning up Ankaios..."

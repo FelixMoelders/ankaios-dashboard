28a68aa1e00161378c9e369d7269a3ede3689bdf
om-oz <165789977+om-oz@users.noreply.github.com>
2024-04-10 11:54:04 +0000

    Implemented some test functions to understand how and which data is sent to API

--- app/DashboardAPI.py
+++ app/DashboardAPI.py
@@ -1,5 +1,6 @@
 from Logger import Logger
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+#from flask_restful import API, Resource, reqparse
 from AnkCommunicationService import AnkCommunicationService
 
 dashboard = Flask(__name__)
@@ -24,13 +25,29 @@ def get_complete_state():
     """
     return ank_comm_service.get_complete_state()
 
-@dashboard.route('/deleteWorkload')
-def send_delete_workload():
+@dashboard.route('/deleteWorkload', methods=['GET', 'POST'])
+def deleteWorkload():
     """
     Returns request to delete a workload as a Json string
     """
-    print("Delete button has been pressed!")
-    return 'Success'
+   # print(workload_names)
+    if request.method == 'POST':
+        if request.json:  # Check for JSON data in request
+            
+            workload_names = request.json.get('workload_names') 
+
+            if workload_names: # Algorithm to check for errors, can be deleted or modified later.
+                print("Delete button has been pressed!")
+                print(workload_names)
+                return jsonify(message= 'OK'), 200
+            else:
+                return jsonify(error="Request is missing 'workload_names'"), 400  
+        else:
+                return jsonify(error="Request did not contain valid JSON data"), 400 
+
+#    print("Delete button has been pressed!")
+#    print(workload_names)
+#    return 'OK', 200
 
 def run(ip="0.0.0.0", p="5001"):
     logger.info(f"Starting the dashboard api ...")

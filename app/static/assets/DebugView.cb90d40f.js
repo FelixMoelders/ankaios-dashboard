61f6f57497371ce5cb47326f6d9b58b5b28de717
Dr. Hendrik Supper <hendrik.supper@d-fine.com>
2024-05-05 21:15:16 +0200

    Built new dist

---
+++ app/static/assets/DebugView.cb90d40f.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.b4618196.js";import{y as t,z as a,A as s,C as o}from"./index.b8ed415a.js";const c=o("h6",null,"This is the content of the Debug tab",-1),u=Object.assign({name:"DebugView"},{__name:"DebugView",setup(n){return(r,_)=>(t(),a(e,{class:"flex flex-center"},{default:s(()=>[c]),_:1}))}});export{u as default};

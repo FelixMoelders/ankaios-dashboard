5e3c502ef956ca5b0ca9a54bf0ccec2888894e56
Dr. Hendrik Supper <hendrik.supper@d-fine.com>
2024-05-05 10:18:11 +0200

    Added logout button and user name to dashboard header if user has successfully logged in ## Added v-if to login button to hide the login button if the user has logged in

--- app/client/src/components/DashboardHeader.vue
+++ app/client/src/components/DashboardHeader.vue
@@ -7,7 +7,9 @@
       <q-space />
 
       <div class="q-gutter-sm row items-center no-wrap">
-        <loginBtn @clickLoginBtn="$emit('clickLoginBtn')" />
+        <loginBtn v-if="!loggedIn" @clickLoginBtn="$emit('clickLoginBtn')" />
+        <div v-if="loggedIn" class="text-white">{{ loggedInUser }}</div>
+        <logoutBtn v-if="loggedIn" @clickLogoutBtn="$emit('clickLogoutBtn')" />
       </div>
     </q-toolbar>
   </q-header>
@@ -16,5 +18,11 @@
 <script setup>
 import ankaiosBtn from "components/AnkaiosButton.vue";
 import loginBtn from "components/LoginButton.vue";
+import logoutBtn from "components/LogoutButton.vue";
 import drawerBtn from "components/DrawerButton.vue";
+
+const props = defineProps({
+  loggedIn: Boolean,
+  loggedInUser: String,
+});
 </script>

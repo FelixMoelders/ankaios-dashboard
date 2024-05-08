4255976ddc33324293781132c7590bb77f093887
Dr. Hendrik Supper <hendrik.supper@d-fine.com>
2024-05-08 23:58:54 +0200

    Formatted donut charts

--- app/client/src/components/HomeView.vue
+++ app/client/src/components/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="row justify-evenly">
     <div class="col-2">
       <q-card square flat bordered class="q-mt-lg">
-        <q-card-section horizontal>
+        <q-card-section horizontal class="bg-grey-2">
           <q-card-section class="bg-secondary">
             <q-icon name="person" size="md" color="white" />
           </q-card-section>
@@ -15,7 +15,7 @@
     </div>
     <div class="col-2">
       <q-card square flat bordered class="q-mt-lg">
-        <q-card-section horizontal>
+        <q-card-section horizontal class="bg-grey-2">
           <q-card-section class="bg-secondary">
             <q-icon name="person" size="md" color="white" />
           </q-card-section>
@@ -28,7 +28,7 @@
     </div>
     <div class="col-2">
       <q-card square flat bordered class="q-mt-lg">
-        <q-card-section horizontal>
+        <q-card-section horizontal class="bg-grey-2">
           <q-card-section class="bg-secondary">
             <q-icon name="person" size="md" color="white" />
           </q-card-section>
@@ -41,7 +41,7 @@
     </div>
     <div class="col-2">
       <q-card square flat bordered class="q-mt-lg">
-        <q-card-section horizontal>
+        <q-card-section horizontal class="bg-grey-2">
           <q-card-section class="bg-secondary">
             <q-icon name="person" size="md" color="white" />
           </q-card-section>
@@ -54,18 +54,35 @@
     </div>
   </div>
 
-  <!-- Anzahl der apexcharts wie gewünscht erweitern -->
-  <div class="row justify-center">
-      <div class="col-2">
-          <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
-      </div>
-      <div class="col-2">
-          <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
-      </div>
+  <div class="row justify-evenly">
+    <div class="col-3">
+      <apexchart
+        class="q-pt-lg"
+        type="donut"
+        :options="chartOptionsDonut1"
+        :series="series"
+      ></apexchart>
+    </div>
+    <div class="col-3">
+      <apexchart
+        class="q-pt-lg"
+        type="donut"
+        :options="chartOptionsDonut2"
+        :series="series"
+      ></apexchart>
+    </div>
+    <div class="col-3">
+      <apexchart
+        class="q-pt-lg"
+        type="donut"
+        :options="chartOptionsDonut3"
+        :series="series"
+      ></apexchart>
+    </div>
   </div>
 
   <div class="row justify-center">
-    <div class="col-12 q-py-xl q-px-xl">
+    <div class="col-12 q-pt-lg q-px-xl">
       <q-table
         title="Workloads"
         flat
@@ -100,77 +117,206 @@ import { ref } from "vue";
 import VueApexCharts from "vue3-apexcharts";
 
 export default {
-    data() {
-        return {
-            filter: ref(""),
-            rows: [
-              {
-                Name: "Frozen Yogurt",
-                Agent: 159,
-                Runtime: 6.0,
-                Dependecies: 24,
-                Tags: 4.0,
-                State: 87,
-              },
-              {
-                Name: "Frozen Yogurt",
-                Agent: 159,
-                Runtime: 6.0,
-                Dependecies: 24,
-                Tags: 4.0,
-                State: 87,
-              },
-              {
-                Name: "Frozen Yogurt",
-                Agent: 159,
-                Runtime: 6.0,
-                Dependecies: 24,
-                Tags: 4.0,
-                State: 87,
-              },
-              {
-                Name: "Frozen Yogurt",
-                Agent: 159,
-                Runtime: 6.0,
-                Dependecies: 24,
-                Tags: 4.0,
-                State: 87,
-              },
-              {
-                Name: "Frozen Yogurt",
-                Agent: 159,
-                Runtime: 6.0,
-                Dependecies: 24,
-                Tags: 4.0,
-                State: 87,
-              },
-            ],
-            series: [44, 55, 41, 17, 15],
-            chartOptions: {
+  data() {
+    return {
+      filter: ref(""),
+      rows: [
+        {
+          Name: "Frozen Yogurt",
+          Agent: 159,
+          Runtime: 6.0,
+          Dependecies: 24,
+          Tags: 4.0,
+          State: 87,
+        },
+        {
+          Name: "Frozen Yogurt",
+          Agent: 159,
+          Runtime: 6.0,
+          Dependecies: 24,
+          Tags: 4.0,
+          State: 87,
+        },
+        {
+          Name: "Frozen Yogurt",
+          Agent: 159,
+          Runtime: 6.0,
+          Dependecies: 24,
+          Tags: 4.0,
+          State: 87,
+        },
+        {
+          Name: "Frozen Yogurt",
+          Agent: 159,
+          Runtime: 6.0,
+          Dependecies: 24,
+          Tags: 4.0,
+          State: 87,
+        },
+        {
+          Name: "Frozen Yogurt",
+          Agent: 159,
+          Runtime: 6.0,
+          Dependecies: 24,
+          Tags: 4.0,
+          State: 87,
+        },
+      ],
+      series: [44, 55, 41, 50, 80],
+      chartOptionsDonut1: {
+        chart: {
+          type: "donut",
+        },
+        legend: { show: false },
+        responsive: [
+          {
+            breakpoint: 1000,
+            options: {
               chart: {
-                type: 'donut',
+                width: 200,
               },
-              responsive: [{
-                breakpoint: 480,
-                options: {
-                  chart: {
-                    width: 200
-                  },
-                  legend: {
-                    position: 'bottom'
-                  }
-                }
-              }]
+              legend: {
+                show: false,
+              },
+              dataLabels: {
+                enabled: false,
+              },
             },
-        }
-    },
-    components: {
-      apexchart: VueApexCharts,
-    },
-}
+          },
+        ],
+        theme: {
+          mode: "light",
+          palette: "palette3",
+          monochrome: {
+            enabled: false,
+            color: "#9C27B0",
+            shadeTo: "light",
+            shadeIntensity: 0.8,
+          },
+        },
+        title: {
+          text: "Workloads per Agent",
+          align: "left",
+          margin: 10,
+          offsetX: 0,
+          offsetY: 0,
+          floating: false,
+          style: {
+            fontSize: "14px",
+            fontWeight: "bold",
+            fontFamily: undefined,
+            color: "#263238",
+          },
+        },
+      },
+      chartOptionsDonut2: {
+        chart: {
+          type: "donut",
+        },
+        legend: { show: false },
+        responsive: [
+          {
+            breakpoint: 1000,
+            options: {
+              chart: {
+                width: 200,
+              },
+              legend: {
+                show: false,
+              },
+              dataLabels: {
+                enabled: false,
+              },
+            },
+          },
+        ],
+        theme: {
+          mode: "light",
+          palette: "palette3",
+          monochrome: {
+            enabled: false,
+            color: "#9C27B0",
+            shadeTo: "light",
+            shadeIntensity: 0.8,
+          },
+        },
+        title: {
+          text: "Workload status",
+          align: "left",
+          margin: 10,
+          offsetX: 0,
+          offsetY: 0,
+          floating: false,
+          style: {
+            fontSize: "14px",
+            fontWeight: "bold",
+            fontFamily: undefined,
+            color: "#263238",
+          },
+        },
+      },
+      chartOptionsDonut3: {
+        chart: {
+          type: "donut",
+        },
+        legend: { show: false },
+        responsive: [
+          {
+            breakpoint: 1000,
+            options: {
+              chart: {
+                width: 200,
+              },
+              legend: {
+                show: false,
+              },
+              dataLabels: {
+                enabled: false,
+              },
+            },
+          },
+        ],
+        theme: {
+          mode: "light",
+          palette: "palette3",
+          monochrome: {
+            enabled: false,
+            color: "#9C27B0",
+            shadeTo: "light",
+            shadeIntensity: 0.8,
+          },
+        },
+        title: {
+          text: "Workload runtimes",
+          align: "left",
+          margin: 10,
+          offsetX: 0,
+          offsetY: 0,
+          floating: false,
+          style: {
+            fontSize: "14px",
+            fontWeight: "bold",
+            fontFamily: undefined,
+            color: "#263238",
+          },
+        },
+      },
+    };
+  },
+  components: {
+    apexchart: VueApexCharts,
+  },
+};
 </script>
 
 <style lang="scss">
+.apexcharts-canvas {
+  border-style: solid;
+  border-width: thin;
+  border-color: $grey-4;
+  background: $grey-1;
+}
+
 .q-table__bottom-item {
   font-size: 10px;
   color: $grey-7;

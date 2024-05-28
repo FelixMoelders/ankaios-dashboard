c3afc0154cac4d877de7106d3f0fcf858af7bac1
Dr. Hendrik Supper <hendrik.supper@d-fine.com>
2024-05-28 21:38:29 +0200

    jira-sdv-165: Moved donut code to separate component HomeViewCharts.vue

--- app/client/src/components/HomeView.vue
+++ app/client/src/components/HomeView.vue
@@ -6,16 +6,11 @@
     :workloadsPerRuntime="workloadsPerRuntime"
   />
 
-  <div class="row justify-evenly">
-    <div v-for="chart in charts" :key="chart.name" class="col-3">
-      <apexchart
-        class="q-pt-lg"
-        ref="chart.name"
-        :options="chart.options"
-        :series="chart.data"
-      ></apexchart>
-    </div>
-  </div>
+  <donutChart
+    :workloadsPerAgent="workloadsPerAgent"
+    :workloadsPerStatus="workloadsPerStatus"
+    :workloadsPerRuntime="workloadsPerRuntime"
+  />
 
   <div class="row justify-center">
     <div class="col-12 q-pt-lg q-px-xl">
@@ -63,23 +58,12 @@ defineOptions({
 });
 
 import { ref, computed, onMounted, onBeforeUnmount } from "vue";
-import apexchart from "vue3-apexcharts";
 import kpi from "components/HomeViewKPI.vue";
+import donutChart from "components/HomeViewCharts.vue";
 
 const filter = ref("");
 var workloadStates = ref([]);
 var desiredState = ref({});
-const donutAgents = ref("");
-const donutStatus = ref("");
-const donutRuntimes = ref("");
-
-const title_donutAgents = "Workloads per Agent";
-const title_donutStatus = "Workload status";
-const title_donutRuntimes = "Workload runtimes";
-
-const chartOptionsAgents = getChartOptions(title_donutAgents);
-const chartOptionsStatus = getChartOptions(title_donutStatus);
-const chartOptionsRuntimes = getChartOptions(title_donutRuntimes);
 
 const workloadsPerRuntime = computed(() => {
   const counter = {};
@@ -148,24 +132,6 @@ const workloadsPerAgent = computed(() => {
   return counter;
 });
 
-var charts = ref([
-  {
-    name: "donutAgents",
-    options: chartOptionsAgents,
-    data: workloadsPerAgent.value ? [] : Object.values(workloadsPerAgent),
-  },
-  {
-    name: "donutStatus",
-    options: chartOptionsStatus,
-    data: workloadsPerStatus.value ? [] : Object.values(workloadsPerStatus),
-  },
-  {
-    name: "donutRuntimes",
-    options: chartOptionsRuntimes,
-    data: workloadsPerRuntime.value ? [] : Object.values(workloadsPerRuntime),
-  },
-]);
-
 const dependencies = computed(() => {
   var list = [];
 
@@ -214,61 +180,6 @@ const rows = computed(() => {
 
   return list;
 });
-
-function getChartOptions(title) {
-  return {
-    chart: {
-      type: "donut",
-      animations: {
-        dynamicAnimation: {
-          enabled: false,
-        },
-      },
-    },
-    legend: { show: false },
-    labels: [],
-    responsive: [
-      {
-        breakpoint: 1000,
-        options: {
-          chart: {
-            width: 200,
-          },
-          legend: {
-            show: false,
-          },
-          dataLabels: {
-            enabled: false,
-          },
-        },
-      },
-    ],
-    theme: {
-      mode: "light",
-      palette: "palette3",
-      monochrome: {
-        enabled: false,
-        color: "#9C27B0",
-        shadeTo: "light",
-        shadeIntensity: 0.8,
-      },
-    },
-    title: {
-      text: title,
-      align: "left",
-      margin: 10,
-      offsetX: 0,
-      offsetY: 0,
-      floating: false,
-      style: {
-        fontSize: "14px",
-        fontWeight: "bold",
-        fontFamily: undefined,
-        color: "#263238",
-      },
-    },
-  };
-}
 
 function aggregateRuntimes(desiredState) {
   const counter = {};
@@ -368,7 +279,6 @@ onMounted(() => {
           }
           // check whether donutAgents, donutStatus, donutRuntimes contain null values and only if not, call updateOptions()
           if (donutAgents.value) {
-            console.log(donutAgents.value);
             donutAgents.value.updateOptions({
               labels: Object.keys(aggregateAgents(workloadStates.value)),
             });
--- app/client/src/components/HomeViewCharts.vue
+++ app/client/src/components/HomeViewCharts.vue
@@ -11,4 +11,102 @@
   </div>
 </template>
 
-<script></script>
+<script setup>
+import { ref, toRef } from "vue";
+import apexchart from "vue3-apexcharts";
+
+const donutAgents = ref("");
+const donutStatus = ref("");
+const donutRuntimes = ref("");
+
+const title_donutAgents = "Workloads per Agent";
+const title_donutStatus = "Workload status";
+const title_donutRuntimes = "Workload runtimes";
+
+const chartOptionsAgents = getChartOptions(title_donutAgents);
+const chartOptionsStatus = getChartOptions(title_donutStatus);
+const chartOptionsRuntimes = getChartOptions(title_donutRuntimes);
+
+const props = defineProps({
+  workloadsPerAgent: Number,
+  workloadsPerStatus: Number,
+  workloadsPerRuntime: Number,
+});
+
+const workloadsPerAgent = toRef(props, "workloadsPerAgent");
+const workloadsPerStatus = toRef(props, "workloadsPerStatus");
+const workloadsPerRuntime = toRef(props, "workloadsPerRuntime");
+
+var charts = ref([
+  {
+    name: "donutAgents",
+    options: chartOptionsAgents,
+    data: workloadsPerAgent.value ? [] : Object.values(workloadsPerAgent),
+  },
+  {
+    name: "donutStatus",
+    options: chartOptionsStatus,
+    data: workloadsPerStatus.value ? [] : Object.values(workloadsPerStatus),
+  },
+  {
+    name: "donutRuntimes",
+    options: chartOptionsRuntimes,
+    data: workloadsPerRuntime.value ? [] : Object.values(workloadsPerRuntime),
+  },
+]);
+
+function getChartOptions(title) {
+  return {
+    chart: {
+      type: "donut",
+      animations: {
+        dynamicAnimation: {
+          enabled: false,
+        },
+      },
+    },
+    legend: { show: false },
+    labels: [],
+    responsive: [
+      {
+        breakpoint: 1000,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            show: false,
+          },
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      },
+    ],
+    theme: {
+      mode: "light",
+      palette: "palette3",
+      monochrome: {
+        enabled: false,
+        color: "#9C27B0",
+        shadeTo: "light",
+        shadeIntensity: 0.8,
+      },
+    },
+    title: {
+      text: title,
+      align: "left",
+      margin: 10,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: "14px",
+        fontWeight: "bold",
+        fontFamily: undefined,
+        color: "#263238",
+      },
+    },
+  };
+}
+</script>
